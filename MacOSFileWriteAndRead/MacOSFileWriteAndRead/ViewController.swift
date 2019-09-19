9aae85d7546148e3ed81662fb32c53b53d570a14
chousigo@fsi.co.jp <chousigo@fsi.co.jp>
2019-09-19 15:46:15 +0900

    コードを追加しました

--- MacOSFileWriteAndRead/MacOSFileWriteAndRead/ViewController.swift
+++ MacOSFileWriteAndRead/MacOSFileWriteAndRead/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let manager = FileManager.default
+        let url = manager.urls(for: .desktopDirectory, in: .userDomainMask).first
+        if let url = url {
+            self.createFile("hello world.txt", url: url)
+        }
+    }
+    
+    private func createFile(_ name: String, url: URL) {
+        let manager = FileManager.default
+        let file = url.appendingPathComponent(name)
+        let exist = manager.fileExists(atPath: file.path)
+        if !exist {
+            let data = Data(base64Encoded: "hello, world", options: .ignoreUnknownCharacters)
+            let result = manager.createFile(atPath: file.path, contents: data, attributes: nil)
+            print("結果 \(result)")
+        }
     }
 
     override var representedObject: Any? {
