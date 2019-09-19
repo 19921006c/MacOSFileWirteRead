a8ff26d57cce07208daac4401e6eb961b65bfc81
chousigo@fsi.co.jp <chousigo@fsi.co.jp>
2019-09-19 15:36:37 +0900

    プロジェクトを作った

---
+++ MacOSFileWriteAndRead/MacOSFileWriteAndRead/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSFileWriteAndRead
+//
+//  Created by chousigou on 2019/09/19.
+//  Copyright © 2019 chousigou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOSFileWriteAndRead/MacOSFileWriteAndReadTests/MacOSFileWriteAndReadTests.swift
@@ -0,0 +1,34 @@
+//
+//  MacOSFileWriteAndReadTests.swift
+//  MacOSFileWriteAndReadTests
+//
+//  Created by chousigou on 2019/09/19.
+//  Copyright © 2019 chousigou. All rights reserved.
+//
+
+import XCTest
+@testable import MacOSFileWriteAndRead
+
+class MacOSFileWriteAndReadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
